1fcd595ef0349e6cd5355665465bfa3998b9e69c
Aihanym <aihanymb968@gmail.com>
2026-04-27 01:41:24 +0500

    e

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-def add(a, b):
-    return a + b
+numbers = [1, 2, 3, 4, 11, 7, 24]
 
-if __name__ == "__main__":
-    print("Result:", add(2, 3))
+list2 = [i * 2 for i in numbers]
+
+taq = [numbers[i] for i in range(len(numbers)) if i % 2 != 0]
+jup = [list2[i] for i in range(len(list2)) if i % 2 == 0]
+
+list3 = [taq[i] + jup[i] for i in range(len(taq))]
+
+print("list1:", numbers)
+print("list2:", list2)
+print("list3:", list3)
