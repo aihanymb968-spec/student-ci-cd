bf00062987864479d7784bb1c5dd31ca4482786b
Aihanym <aihanymb968@gmail.com>
2026-04-27 01:45:07 +0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,4 +1,9 @@
-from app import add
+from app import process_lists
 
-def test_add():
-    assert add(2, 3) == 5
+def test_process_lists():
+    numbers = [1, 2, 3, 4, 11, 7, 24]
+
+    list2, list3 = process_lists(numbers)
+
+    assert list2 == [2, 4, 6, 8, 22, 14, 48]
+    assert list3 == [4, 10, 29]
